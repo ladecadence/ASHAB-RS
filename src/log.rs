8cad0c980cefbc1c4f63f27fefadc8028461ecb3
David Pello <dpello@ladecadence.net>
2017-01-11 16:50:53 +0000

    Logging

---
+++ src/log.rs
@@ -0,0 +1,58 @@
+extern crate chrono;
+
+use chrono::*;
+use std::io::prelude::*;
+use std::fs::OpenOptions;
+
+
+pub enum LogType {
+	Data,
+	Info,
+	Warn,
+	Err,
+}
+
+pub struct Log {
+	pub filename: String,
+}
+
+
+impl Log {
+	pub fn new (f: &str) -> Log {
+		Log {
+			filename: String::from(f),
+		}
+
+	}
+
+	pub fn init(&mut self) {
+		// create new file or erase if it exists
+		let f = OpenOptions::new()
+			.create(true)
+			.truncate(true)
+			.write(true)
+			.open(self.filename.as_str()).unwrap();
+	}
+
+	pub fn log (&mut self, t: LogType, msg: &str) {
+		// open file for append
+		let mut f = OpenOptions::new()
+			.append(true)
+			.create(true)
+			.open(self.filename.as_str()).unwrap();
+		// log msg
+		//match t {
+		//	LogType::Data => f.write_all(b"DATA::").unwrap(),
+		//	LogType::Info => f.write_all(b"INFO::").unwrap(),
+		//	LogType::Warn => f.write_all(b"WARN::").unwrap(),
+		//	LogType::Err  => f.write_all(b" ERR::").unwrap(),
+		//}
+		f.write_all(UTC::now().to_rfc3339().as_bytes()).unwrap();
+		f.write_all(b":: ").unwrap();
+		f.write_all(msg.as_bytes()).unwrap();
+		f.write_all(b"\n").unwrap();
+	}
+}
+
+
+
--- src/test.rs
+++ src/test.rs
@@ -1,8 +1,7 @@
 extern crate serial;
 extern crate spidev;
 extern crate sysfs_gpio;
-
-use std::io::prelude::*;
+extern crate chrono;
 
 mod gps;
 use gps::*;
@@ -16,10 +15,13 @@ use rf95::*;
 mod ds18b20;
 use ds18b20::*;
 
+mod log;
+use log::*;
+
 // CONFIGURATION
 /////////////////
 
-static GPS_PORT_NAME: &'static str = "/dev/ttyUSB0";
+static GPS_PORT_NAME: &'static str = "/dev/ttyAMA0";
 
 const GPS_PORT_SETTINGS: serial::PortSettings = serial::PortSettings {
     baud_rate:    serial::Baud9600,
@@ -39,9 +41,9 @@ fn main() {
     let mut gps: GPS =  GPS::new(GPS_PORT_NAME);
     gps.config(GPS_PORT_SETTINGS);
    
-    gps.update();
-    println!("{}", if gps.sats >=4 { "GPS FIX OK" } else { "NO GPS FIX" });
-    println!("{}N, {}W", gps.decimal_latitude(), gps.decimal_longitude());
+    //gps.update();
+    //println!("{}", if gps.sats >=4 { "GPS FIX OK" } else { "NO GPS FIX" });
+    //println!("{}N, {}W", gps.decimal_latitude(), gps.decimal_longitude());
 
     // test Image
     let mut img: Image = Image::new(0, "ssdv", "/home/pi");
@@ -69,8 +71,16 @@ fn main() {
     //lora.send("$$TELEMETRY TEST".as_bytes());
     //lora.wait_packet_sent();
 
+    // test temperature sensor
     let mut temp_sensor: DS18B20 = DS18B20::new("28-031682a91bff");
     println!("Temperature: {}", temp_sensor.read());
+
+    // test log
+    let mut log: Log = Log::new("prueba.txt");
+
+    log.init();
+    log.log(LogType::Info, "Probando Log");
+    log.log(LogType::Warn, "Advertencia!!!");
 
     std::process::exit(0);
 }
