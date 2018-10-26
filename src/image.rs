30871fc5983e130c264b03c5d8aebab49651081a
David Pello <dpello@ladecadence.net>
2018-10-26 12:08:16 +0000

    added date to filename and return a Result<> when taking a pic

--- src/image.rs
+++ src/image.rs
@@ -1,4 +1,8 @@
 use std::process::Command;
+use std::io::{Error, ErrorKind};
+
+extern crate chrono;
+use chrono::prelude::*;
 
 static STILL_PROGRAM: &'static str = "raspistill";
 
@@ -22,14 +26,17 @@ impl Image {
 	}
 
 	fn update_name(&mut self) {
-		self.filename = self.path.clone() + "/" + &self.basename + &self.number.to_string() + ".jpg";
+	    // update name with pic number and current time
+		self.filename = self.path.clone() + "/" + &self.basename
+                    + "-" + &Utc::now().to_rfc3339().to_string() 
+                    + "-" + &self.number.to_string() + ".jpg";
 	}
 
-	pub fn capture(&mut self) -> bool {
+	pub fn capture(&mut self) -> Result<(), Error> {
 		
 		// update filename
 		self.update_name();
-		
+
 		let status = Command::new(STILL_PROGRAM)
 				.arg("-st")
 				.arg("-t")
@@ -40,17 +47,17 @@ impl Image {
 		let exit_code: i32;
 		match status {
 			Ok(s) => exit_code = s.code().unwrap(),
-			Err(e) => { println!("{}", e); return false }
+			Err(e) => { println!("{}", e); return Err(Error::new(ErrorKind::NotFound, "raspistill failed")) }
 		}
 		
 		// if we manage to capture a picture, increment filename number		
 		if exit_code == 0 {
 			self.number = self.number + 1;
-			return true;
+			return Ok(());
 		}
 		
 		// exit code was not 0
-		false
+		Err(Error::new(ErrorKind::NotFound, "Can't take picture"))
 		
 	}
 }
