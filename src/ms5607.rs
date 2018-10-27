540cb88acebc747dfb8b56da5f9df1e177b37a95
David Pello <dpello@ladecadence.net>
2018-10-27 18:10:50 +0200

    compilable test

--- src/ms5607.rs
+++ src/ms5607.rs
@@ -20,27 +20,28 @@ const MS5607_CMD_PROM_RD: u8    = 0xA0;    // readout of PROM registers
 
 struct Ms5607 {
     pub bus: LinuxI2CDevice,
-    pub addr: u8,
+    pub addr: u16,
     pub prom: [u16; 7],
 }
 
 impl Ms5607 {
-    pub fn new(b: u8, a: u8) -> Ms5607 {
+    pub fn new(b: u8, a: u16) -> Ms5607 {
         Ms5607 {
-            bus: LinuxI2CDevice::new(format!("/dev/i2c-{}", b), a)?,
+            bus: LinuxI2CDevice::new(format!("/dev/i2c-{}", b), a).unwrap(),
             addr: a,
-            buf: [0,0,0,0,0,0,0],
+            prom: [0,0,0,0,0,0,0],
         }
     }
 
     pub fn read_prom(&mut self) -> Result<(), &'static str> {
-        self.bus.smbus_write_byte_data(0x00, MS5607_CMD_RESET)?;
+        self.bus.smbus_write_byte_data(0x00, MS5607_CMD_RESET).unwrap();
         thread::sleep(Duration::from_millis(30));
         for i in 0..7 {
             self.prom[i] = 0x0000;
-            self.prom[i] = self.bus.smbus_read_word_data();
+            self.prom[i] = self.bus.smbus_read_word_data(MS5607_CMD_PROM_RD+(2*i as u8)).unwrap();
         }
 
+        Ok(())
     }
 }
 
