4041cf69dbc6513674bba8be1d5737f19812089b
David Pello <dpello@ladecadence.net>
2018-10-29 17:22:43 +0000

    temperature and pressure reading

--- src/ms5607.rs
+++ src/ms5607.rs
@@ -18,10 +18,13 @@ const MS5607_CMD_ADC_2048: u8   = 0x06;    // ADC oversampling ratio to 2048
 const MS5607_CMD_ADC_4096: u8   = 0x08;    // ADC oversampling ratio to 4096
 const MS5607_CMD_PROM_RD: u8    = 0xA0;    // readout of PROM registers
 
-struct Ms5607 {
+#[allow(dead_code)]
+pub struct Ms5607 {
     pub bus: LinuxI2CDevice,
     pub addr: u16,
     pub prom: [u16; 7],
+    temp: i64,
+    p: i64,
 }
 
 impl Ms5607 {
@@ -30,18 +33,95 @@ impl Ms5607 {
             bus: LinuxI2CDevice::new(format!("/dev/i2c-{}", b), a).unwrap(),
             addr: a,
             prom: [0,0,0,0,0,0,0],
+            temp: 0,
+            p: 0,
         }
     }
 
     pub fn read_prom(&mut self) -> Result<(), &'static str> {
         self.bus.smbus_write_byte_data(0x00, MS5607_CMD_RESET).unwrap();
         thread::sleep(Duration::from_millis(30));
+        let mut temp: [u8; 2] = [0, 0];
         for i in 0..7 {
             self.prom[i] = 0x0000;
-            self.prom[i] = self.bus.smbus_read_word_data(MS5607_CMD_PROM_RD+(2*i as u8)).unwrap();
+            self.bus.write(&[MS5607_CMD_PROM_RD+(2*i as u8)]).unwrap();
+            self.bus.read(&mut temp).unwrap();
+            self.prom[i] = (temp[0] as u16) << 8;
+            self.prom[i] = self.prom[i] + temp[1] as u16;
+
         }
+        //println!("Prom: {:?}", self.prom);
 
         Ok(())
+    }
+
+    pub fn read_adc(&mut self, cmd: u8) -> Result<i64, &'static str> {
+        let mut value: i64 = 0;
+
+    
+        self.bus.smbus_write_byte_data(MS5607_CMD_ADC_CONV+cmd, 0).unwrap();
+
+        match cmd & 0x0f {
+            MS5607_CMD_ADC_256 => thread::sleep(Duration::from_millis(1)),
+            MS5607_CMD_ADC_512 => thread::sleep(Duration::from_millis(3)),
+            MS5607_CMD_ADC_1024 => thread::sleep(Duration::from_millis(4)),
+            MS5607_CMD_ADC_2048 => thread::sleep(Duration::from_millis(6)),
+            MS5607_CMD_ADC_4096 => thread::sleep(Duration::from_millis(10)),
+            _ => thread::sleep(Duration::from_millis(10)),
+        }
+
+        let mut data: [u8; 3] = [0, 0, 0];
+        self.bus.write(&[MS5607_CMD_ADC_READ]).unwrap();
+        
+        self.bus.read(&mut data).unwrap();
+       
+        value = ((data[0] as i64) << 16) + ((data[1] as i64) << 8) + data[2] as i64;
+
+        Ok(value)
+    }
+
+    pub fn update(&mut self) -> Result<(), &'static str> {
+        let d2: i64 = self.read_adc(MS5607_CMD_ADC_D2+MS5607_CMD_ADC_4096).unwrap();
+        let d1: i64 = self.read_adc(MS5607_CMD_ADC_D1+MS5607_CMD_ADC_4096).unwrap();
+
+        // calculate 1st order pressure and temperature (MS5607 1st order algorithm)
+        let dt: i64 = d2 - self.prom[5] as i64 * (2_i64.pow(8));
+        let mut off: i64  = self.prom[2] as i64 * (2_i64.pow(17))+dt*self.prom[4] as i64 /(2_i64.pow(6));
+        let mut sens: i64 = self.prom[1] as i64 * (2_i64.pow(16))+dt*self.prom[3] as i64 /(2_i64.pow(7));
+        self.temp = 2000 + (dt * self.prom[6] as i64 )/(2_i64.pow(23));
+        self.p = ((d1*sens)/(2_i64.pow(21))-off)/(2_i64.pow(15));
+        
+        let mut t2: i64 = 0;
+        let mut off2: i64 = 0;
+        let mut sens2: i64 = 0;
+
+        // perform higher order corrections
+        if self.temp < 2000 {
+            t2=dt*dt/(2_i64.pow(31));
+            off2=61*(self.temp-2000)*(self.temp-2000)/(2_i64.pow(4));
+            sens2=2*(self.temp-2000)*(self.temp-2000);
+
+            if self.temp < -1500 {
+                off2=off2+(15*(self.temp+1500)*(self.temp+1500));
+                sens2=sens2+(8*(self.temp+1500)*(self.temp+1500));
+            }
+        }
+
+        self.temp = self.temp-t2;
+        off = off - off2;
+        sens = sens - sens2;
+
+        self.p=((d1*sens)/(2_i64.pow(21))-off)/(2_i64.pow(15));
+
+        Ok(())
+    }
+
+    pub fn get_temp(&mut self) -> Result<f32, &'static str> {
+        return Ok(self.temp as f32 / 100.0);
+    }
+
+    pub fn get_pres(&mut self) -> Result<f32, &'static str> {
+        return Ok(self.p as f32 / 100.0);
     }
 }
 
