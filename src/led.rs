f04bc3658f206c4a0fc174e306222417e1989cae
David Pello <zako@ladecadence.net>
2019-09-18 12:44:28 +0200

    license info, static led on error

--- src/led.rs
+++ src/led.rs
@@ -1,3 +1,21 @@
+// (C) 2018 David Pello Gonzalez for ASHAB
+//
+// This program is free software: you can redistribute it
+// and/or modify it under the terms of the GNU General Public License
+// as published by the Free Software Foundation, either version 2
+// of the License, or (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.
+// If not, see <http://www.gnu.org/licenses/>.
+
+// Status LED control
+
 extern crate sysfs_gpio;
 use std::thread;
 use std::time::Duration;
@@ -36,12 +54,14 @@ impl LED {
         Ok(())
     }
 
+    // fast blink
     pub fn blink(&mut self) {
         self.pin.set_value(1).unwrap();
         thread::sleep(Duration::from_millis(1));
         self.pin.set_value(0).unwrap();
     }
 
+    // error
     pub fn err(&mut self) {
         for _i in 0..5 {
             self.pin.set_value(1).unwrap();
@@ -49,5 +69,7 @@ impl LED {
             self.pin.set_value(0).unwrap();
             thread::sleep(Duration::from_millis(1));
         }
+        // keep it on
+        self.pin.set_value(1).unwrap();
     }
 }
